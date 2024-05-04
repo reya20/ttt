8f9c5c4774a928397218181cd2be6ada2b983e25
reya20 <168893411+reya20@users.noreply.github.com>
2024-05-04 15:56:28 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -4,20 +4,18 @@ const yesBtn = document.querySelector(".yes-btn")
 const noBtn = document.querySelector(".no-btn")
 
 yesBtn.addEventListener("click", ()=> {
-    question.innerHTML = "yeyyy I Love you so much";
+    question.innerHTML = " I Love ya so much";
     gif.src="https://media.tenor.com/O0L78rE1EZQAAAAC/milk-and-mocha-cute.gif"
 })
 
-noBtn.addEventListener("mouseover", () => {
+noBtn.addEventListener("mouseover", ()=> {
     const noBtnRect =noBtn.getBoundingClientRect();
     const maxX = Window.innerWidth - noBtnRect.width;
     const maxY = Window.innerHeight - noBtnRect.height;
 
     const randomX = Math.floor(Math.random() * maxX)
-    const randomY = Math.floor(Math.random() * maxY)
+    const randomy = Math.floor(Math.random() * maxY)
 
     noBtn.style.left =randomX + "px";
     noBtn.style.top =randomX + "px";
-
-     
 })
